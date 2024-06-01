1a976a943d6845670e73b9399be9b94f511f12d0
Shumimran <164326671+Shumimran@users.noreply.github.com>
2024-06-01 12:05:33 +0500

    Add files via upload
    Aouther: Shumaila Imran

---
+++ app.js
@@ -0,0 +1,4 @@
+// Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. 
+// Include at least three different activities.
+var MyIdealDay = "My ideal day would involve:\n1. waking up early and going for a morning walk.\n2. Spending a few hours in coding on my personal project.\n3. Ending the day by reading a good book.";
+console.log(MyIdealDay);
